84a2e70b638d011dcb5411f50eb4a5f74f4ed813
alex <arodriguez202020@gmail.com>
2021-07-21 13:36:50 -0500

    create seperate index page to display all posts on seperate page, add route path to index.js src/router, create router link in in app.vue file

---
+++ src/views/PostIndex.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <h1>All Posts</h1>
+    <div v-for="post in posts" v-bind:key="post.id">
+      <h2>{{ post.title }}</h2>
+      <p>body: {{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      posts: [],
+    };
+  },
+  created: function () {
+    this.indexPosts();
+  },
+  methods: {
+    indexPosts: function () {
+      axios.get("/posts").then((response) => {
+        console.log("posts index", response);
+        this.posts = response.data;
+      });
+    },
+  },
+};
+</script>
