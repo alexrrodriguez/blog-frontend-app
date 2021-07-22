257a57dd7b15b4a3f6d95756e475aa01493ed221
alex <arodriguez202020@gmail.com>
2021-07-22 13:32:40 -0500

    update router link to wrap around title, body, and image to postShow.vue page

--- src/views/PostIndex.vue
+++ src/views/PostIndex.vue
@@ -2,10 +2,11 @@
   <div class="posts-index">
     <h1>All Posts</h1>
     <div v-for="post in posts" v-bind:key="post.id">
-      <h2>{{ post.title }}</h2>
-      <p>{{ post.body }}</p>
-      <img v-bind:src="post.image" alt="post.title" />
-      <router-link v-bind:to="`/posts/${post.id}`">More details</router-link>
+      <router-link v-bind:to="`/posts/${post.id}`">
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.body }}</p>
+        <img v-bind:src="post.image" alt="post.title" />
+      </router-link>
     </div>
   </div>
 </template>
