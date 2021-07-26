90b2403216cc38e882e1573c30215b50e87e29d2
alex <arodriguez202020@gmail.com>
2021-07-26 12:41:59 -0500

    install and import bootstrap, add background img and font to app.vue,  add navbar and card to app.vue and postIndex.vue

--- src/views/PostIndex.vue
+++ src/views/PostIndex.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="posts-index">
     <h1>All Posts</h1>
+    <div class="card" style="width: 18rem" v-for="post in posts" v-bind:key="post.id">
+      <img v-bind:src="post.image" alt="post.title" class="card-img-top" />
+      <div class="card-body">
+        <h5 class="card-title">{{ post.title }}</h5>
+        <p class="card-text">
+          {{ post.body }}
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
     <div v-for="post in posts" v-bind:key="post.id">
       <router-link v-bind:to="`/posts/${post.id}`">
         <h2>{{ post.title }}</h2>
@@ -11,7 +21,11 @@
   </div>
 </template>
 
-<style></style>
+<style>
+body {
+  font-family: Lucida Bright, Georgia, serif;
+}
+</style>
 
 <script>
 import axios from "axios";
